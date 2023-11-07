2a22e47ba1a328d62f5703ac0d7d0d85b6305219
AprendizBR45 <129765156+AprendizBR45@users.noreply.github.com>
2023-11-07 19:41:33 -0400

    Add files via upload

---
+++ video 1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace videos_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Este es un Programa de HOla mundo 
+            /* Este es un comentario 
+              de bloque */
+            Console.WriteLine("Hola Mundo");
+            Console.ReadKey();
+        }
+    }
+}
