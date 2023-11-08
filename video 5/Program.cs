a7b258b48be324f7e5408158f38447131ac444a7
AprendizBR45 <129765156+AprendizBR45@users.noreply.github.com>
2023-11-07 20:00:12 -0400

    Add files via upload

---
+++ video 5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace video_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            clsEmpleados empleado;
+            empleado = new clsEmpleados();
+            empleado.Edad = 25;
+            empleado.Nombre = "Angel Suarez";
+            empleado.SueldoDiario = 12.5m;
+            decimal total;
+            total = empleado.CalculaSalario(30);
+            Console.WriteLine("El salario mensual del empleado " + empleado.Nombre);
+            Console.WriteLine("es: " + total.ToString("C"));
+            Console.ReadKey();
+
+        }
+    }
+}
